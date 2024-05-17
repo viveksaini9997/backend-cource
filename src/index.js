02f9cac0afa3279b191d46539e58530c701682e3
Vivek <wwwviveksaini701@gmail.com>
2024-05-17 08:52:45 -0800

    utils code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 // require('dotenv').config({path:"./env"})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDb from "./db/index.js";
 
-
 dotenv.config({
-    path:"./env"
-})
+  path: "./env",
+});
 
 connectDb()
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Mongodb is running ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection error:", err);
+  });
 
 /*
 import express from "express"
